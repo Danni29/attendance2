ffdc89190b2fad75ade8b04e2d923243de838b02
Do <Ngocdo012340@gmail.com>
2021-09-20 14:57:07 +0700

    update

---
+++ Assignment/ModelView/SubjectsOfClassModelView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Assignment.DB;
+
+namespace Assignment.ModelView
+{
+    public class SubjectsOfClassModelView
+    {
+        IEnumerable<String> stringsFound;
+        public IEnumerable<SubjectsOfClass> SubjectsOfClasses { get; set; }
+
+        public IEnumerable<String>  SlotTime { get; set; }
+    }
+
+}
