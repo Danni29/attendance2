a71820c7f7a4a40660fd4dde161aa60e5f23b5be
Do <Ngocdo012340@gmail.com>
2021-09-21 18:05:51 +0700

    update

--- Assignment/DB/DBModel.Designer.cs
+++ Assignment/DB/DBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\dinh9\OneDrive\Desktop\attendance2\Assignment\DB\DBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\A19052\attendance2\Assignment\DB\DBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
